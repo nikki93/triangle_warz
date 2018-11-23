09d90d40245cc3aa56a095e1ddc02ed2df0a2537
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-11-22 20:43:43 -0800

    bullets / shooting

--- server.lua
+++ server.lua
@@ -16,8 +16,8 @@ function server.load()
     share.bullets = {}
 end
 
-function server.connect(id)
-    share.triangles[id] = {
+function server.connect(clientId)
+    share.triangles[clientId] = {
         x = math.random(0, W),
         y = math.random(0, H),
         r = math.random(),
@@ -25,21 +25,21 @@ function server.connect(id)
         b = math.random(),
         targetX = 0,
         targetY = 0,
+        shootTimer = 0, -- Can shoot if <= 0
     }
 end
 
-function server.disconnect(id)
+function server.disconnect(clientId)
 end
 
-function server.update(dt)
-    -- Update triangles
-    for id, tri in pairs(share.triangles) do
-        local home = homes[id]
-        if home.move then -- Info may have not arrived yet
-            -- Set target
-            tri.targetX, tri.targetY = home.targetX, home.targetY
+local nextBulletId = 1 -- For choosing bullet ids
 
-            -- Move
+function server.update(dt)
+    -- Triangles
+    for clientId, tri in pairs(share.triangles) do
+        local home = homes[clientId]
+        if home.move then -- Info may have not arrived yet
+            -- Moving
             local move = home.move
             local vx, vy = 0, 0
             if move.up then vy = vy - 220 end
@@ -49,10 +49,43 @@ function server.update(dt)
             local vLen = math.sqrt(vx * vx + vy * vy)
             if vLen > 0 then vx, vy = 220 * vx / vLen, 220 * vy / vLen end -- Limit speed
             tri.x, tri.y = tri.x + vx * dt, tri.y + vy * dt
-            if tri.x < 0 then tri.x = tri.x + W end
-            if tri.x > W then tri.x = tri.x - W end
-            if tri.y < 0 then tri.y = tri.y + H end
-            if tri.y > H then tri.y = tri.y - H end
+            tri.x, tri.y = math.max(0, math.min(tri.x, W)), math.max(0, math.min(tri.y, H))
+
+            -- Targeting
+            tri.targetX, tri.targetY = home.targetX, home.targetY
+
+            -- Shooting
+            if tri.shootTimer > 0 then -- Tick the shoot timer
+                tri.shootTimer = tri.shootTimer - dt
+            end
+            if tri.shootTimer <= 0 and home.wantShoot then -- Can and want to shoot? Shoot!
+                local dirX, dirY = tri.targetX - tri.x, tri.targetY - tri.y
+                if dirX == 0 and dirY == 0 then dirX = 1 end -- Prevent division by zero
+                local dirLen = math.sqrt(dirX * dirX + dirY * dirY)
+                dirX, dirY = dirX / dirLen, dirY / dirLen
+                share.bullets[nextBulletId] = { -- Create the bullet
+                    owner = clientId,
+                    x = tri.x + 30 * dirX,
+                    y = tri.y + 30 * dirY,
+                    dirX = dirX,
+                    dirY = dirY,
+                    r = 1.5 * tri.r,
+                    g = 1.5 * tri.g,
+                    b = 1.5 * tri.b,
+                    lifetime = 1,
+                }
+                nextBulletId = nextBulletId + 1
+                tri.shootTimer = 0.2
+            end
+        end
+    end
+
+    -- Bullets
+    for bulId, bul in pairs(share.bullets) do
+        bul.x, bul.y = bul.x + 800 * bul.dirX * dt, bul.y + 800 * bul.dirY * dt
+        bul.lifetime = bul.lifetime - dt
+        if bul.lifetime <= 0 then
+            share.bullets[bulId] = nil
         end
     end
 end
