9c9eaae8f928f64662bb0dbd601740ac9bda37ae
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-11-22 20:59:06 -0800

    being shot, health

--- server.lua
+++ server.lua
@@ -1,8 +1,6 @@
 require 'libs'
 local server = cs.server
 
-local serpent = require 'https://raw.githubusercontent.com/pkulchenko/serpent/522a6239f25997b101c585c0daf6a15b7e37fad9/src/serpent.lua'
-
 local W, H = 800, 600 -- Game world size
 
 server.enabled = true
@@ -12,11 +10,13 @@ local share = server.share
 local homes = server.homes
 
 function server.load()
+    share.scores = {}
     share.triangles = {}
     share.bullets = {}
 end
 
 function server.connect(clientId)
+    share.scores[clientId] = 0
     share.triangles[clientId] = {
         x = math.random(0, W),
         y = math.random(0, H),
@@ -26,10 +26,13 @@ function server.connect(clientId)
         targetX = 0,
         targetY = 0,
         shootTimer = 0, -- Can shoot if <= 0
+        health = 100,
     }
 end
 
 function server.disconnect(clientId)
+    share.scores[clientId] = nil
+    share.triangles[clientId] = nil
 end
 
 local nextBulletId = 1 -- For choosing bullet ids
@@ -64,7 +67,7 @@ function server.update(dt)
                 local dirLen = math.sqrt(dirX * dirX + dirY * dirY)
                 dirX, dirY = dirX / dirLen, dirY / dirLen
                 share.bullets[nextBulletId] = { -- Create the bullet
-                    owner = clientId,
+                    ownerClientId = clientId,
                     x = tri.x + 30 * dirX,
                     y = tri.y + 30 * dirY,
                     dirX = dirX,
@@ -76,6 +79,44 @@ function server.update(dt)
                 }
                 nextBulletId = nextBulletId + 1
                 tri.shootTimer = 0.2
+            end
+
+            -- Check if we got shot...
+            local nang = -math.atan2(tri.targetY - tri.y, tri.targetX - tri.x)
+            local sin, cos = math.sin(nang), math.cos(nang)
+            for bulId, bul in pairs(share.bullets) do
+                if bul.ownerClientId ~= clientId then -- Don't get shot by own bullet...
+                    local dx, dy = bul.x - tri.x, bul.y - tri.y
+                    local hitX, hitY
+                    if dx * dx + dy * dy < 3600 then -- Ignore if far
+                        for i = -1, 1, 0.2 do -- Check a few points to prevent 'tunneling'
+                            -- Isosceles triangle point membership math...
+                            local bx, by = bul.x + 18 * i * bul.dirX, bul.y + 18 * i * bul.dirY
+                            local dx, dy = bx - tri.x, by - tri.y
+                            local rdx, rdy = dx * cos - dy * sin, dx * sin + dy * cos
+                            if rdx > -20 then
+                                rdx = rdx + 20
+                                rdy = math.abs(rdy)
+                                if rdx / 50 + rdy / 20 < 1 then
+                                    hitX, hitY = bx, by
+                                    break
+                                end
+                            end
+                        end
+                    end
+                    if hitX then -- We got shot!
+                        share.bullets[bulId] = nil
+                        tri.health = tri.health - 5
+                        if tri.health <= 0 then -- We died!
+                            tri.health = 100
+                            tri.x, tri.y = math.random(10, W - 10), math.random(10, H - 10)
+                            local shooterScore = share.scores[bul.ownerClientId]
+                            if shooterScore then
+                                share.scores[bul.ownerClientId] = shooterScore + 1
+                            end
+                        end
+                    end
+                end
             end
         end
     end
